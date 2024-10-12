930287b23a753e78d9d45c5b4ffd899c1eba8f22
Vishal Vishwakarma <103165761+VishalVish740@users.noreply.github.com>
2024-10-12 16:42:48 +0530

    Saved

--- src/App.js
+++ src/App.js
@@ -20,14 +20,14 @@ const App = () => {
           height={3}
         />
         <Switch>
-          <Route exact path="/"><News setProgress={setProgress} key="general" pageSize={pageSize} country="in" category="general" /></Route>
-          <Route exact path="/business"><News setProgress={setProgress} key="business" pageSize={pageSize} country="in" category="business" /></Route>
-          <Route exact path="/entertainment"><News setProgress={setProgress} key="entertainment" pageSize={pageSize} country="in" category="entertainment" /></Route>
-          <Route exact path="/general"><News setProgress={setProgress} key="general" pageSize={pageSize} country="in" category="general" /></Route>
-          <Route exact path="/health"><News setProgress={setProgress} key="health" pageSize={pageSize} country="in" category="health" /></Route>
-          <Route exact path="/science"><News setProgress={setProgress} key="science" pageSize={pageSize} country="in" category="science" /></Route>
-          <Route exact path="/sports"><News setProgress={setProgress} key="sports" pageSize={pageSize} country="in" category="sports" /></Route>
-          <Route exact path="/technology"><News setProgress={setProgress} key="technology" pageSize={pageSize} country="in" category="technology" /></Route>
+          <Route exact path="/"><News setProgress={setProgress} key="general" pageSize={pageSize} country="us" category="general" /></Route>
+          <Route exact path="/business"><News setProgress={setProgress} key="business" pageSize={pageSize} country="us" category="business" /></Route>
+          <Route exact path="/entertainment"><News setProgress={setProgress} key="entertainment" pageSize={pageSize} country="us" category="entertainment" /></Route>
+          <Route exact path="/general"><News setProgress={setProgress} key="general" pageSize={pageSize} country="us" category="general" /></Route>
+          <Route exact path="/health"><News setProgress={setProgress} key="health" pageSize={pageSize} country="us" category="health" /></Route>
+          <Route exact path="/science"><News setProgress={setProgress} key="science" pageSize={pageSize} country="us" category="science" /></Route>
+          <Route exact path="/sports"><News setProgress={setProgress} key="sports" pageSize={pageSize} country="us" category="sports" /></Route>
+          <Route exact path="/technology"><News setProgress={setProgress} key="technology" pageSize={pageSize} country="us" category="technology" /></Route>
         </Switch>
       </Router>
     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -23,6 +23,10 @@ const NavBar = () => {
                             <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
                             <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
                         </ul>
+                        <form className="d-flex">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                                <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form>
                     </div>
                 </div>
             </nav>
